7c0e6ca80c531f297aa3d9e18471fb3a30ded901
Matt Stibbs <git@stibbsy.co.uk>
2016-02-25 15:35:24 +0000

    Add applications install script for homebrew packages

---
+++ applications.sh
@@ -0,0 +1,38 @@
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew doctor
+brew upgrade
+
+## Terminal Tools
+brew install bash-completion 
+brew install wget
+
+## Helpful Stuff
+brew cask install licecap
+brew cask install tripmode
+
+## Security Stuff
+brew install ssh-copy-id
+brew install keybase
+brew install openssl
+
+## Python Stuff
+brew install python
+brew install python3
+
+## Development Tools
+brew install heroku-toolbelt
+brew install postgresql
+brew install libevent
+brew install libmemcached
+brew install memcached
+brew install redis
+brew install sqlite
+
+## Communication Tools
+brew cask install skype 
+brew cask install slack
+
+## Multimedia Stuff
+brew install youtube-dl
