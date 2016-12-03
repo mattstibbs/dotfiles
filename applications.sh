ea4aee1b205f709e8d2b1a1cc9257a4a12a13c25
Matt Stibbs <git@stibbsy.co.uk>
2016-12-03 22:52:14 +0000

    Update application install list

--- applications.sh
+++ applications.sh
@@ -5,20 +5,18 @@ brew doctor
 brew upgrade
 
 ## Terminal Tools
-brew install bash-completion 
+brew install bash-completion
 brew install wget
 
 ## Helpful Stuff
 brew cask install licecap
-brew cask install tripmode
 brew cask install cronnix
 
-## Security Stuff
+## System Security Tools
 brew install ssh-copy-id
-brew install keybase
 brew install openssl
 
-## Python Stuff
+## Python
 brew install python
 brew install python3
 
@@ -27,16 +25,32 @@ brew install heroku-toolbelt
 brew install postgresql
 brew install libevent
 brew install libmemcached
-brew install memcached
 brew install redis
 brew install sqlite
 
+## IDEs / Editors
+brew cask install atom
+brew cask install pycharm
+brew cask install webstorm
+brew cask install datagrip
+brew cask install xmplify
+
+## Python Tools
+brew install autoenv
+
 ## Communication Tools
-brew cask install skype 
+brew cask install skype
 brew cask install slack
+Brew cask install adium
 
 # Productivity Tools
-brew cask install doitim
+brew cask install doitim # Task manager
+brew cask install evernote
+
+# System Tools
+brew cask install path-finder # Alternative file browser app
+brew cask install flux # Automatically reduces blue light at night
+brew cask install tripmode # For restricting bandwidth usage when on hotspots
 
 ## Multimedia Stuff
-brew install youtube-dl
+#brew install youtube-dl
